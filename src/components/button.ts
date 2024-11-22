1746dff98b514bb07113a57e1470646ff7295d28
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-11-22 15:07:57 +0100

    feat: add button component

---
+++ src/components/button.ts
@@ -0,0 +1,6 @@
+export function createButton() {
+  const button = document.createElement('button');
+  button.className = 'btn text-xs sm:text-sm px-[28px] py-[12px] sm:px-10 sm:py-4';
+
+  return button;
+}
