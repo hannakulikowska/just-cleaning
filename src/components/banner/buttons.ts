9df715b8e5f09fe123b1d89f7d67874e166614a9
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-11-22 18:13:11 +0100

    refactor: update to use new createButton structure with object arguments

--- src/components/banner/buttons.ts
+++ src/components/banner/buttons.ts
@@ -3,12 +3,19 @@ import { createButton } from '../button';
 export function createButtons() {
   const buttonsContainer = document.createElement('div');
   buttonsContainer.className = 'flex gap-4 sm:gap-6 mt-6 sm:mt-10';
-  const buttonOffer = createButton();
-  const buttonContact = createButton();
-  buttonOffer.classList.add('btn-primary', 'hidden', 'xs:inline');
-  buttonContact.classList.add('btn-white', 'hidden', 'xs:inline');
-  buttonOffer.textContent = 'Oferta';
-  buttonContact.textContent = 'Kontakt';
+
+  const buttonOffer = createButton(
+    'Oferta',
+    '/offer',
+    'btn-white hidden xs:inline',
+    'Sprawdź naszą ofertę'
+  );
+  const buttonContact = createButton(
+    'Kontakt',
+    '/contact',
+    'btn-primary hidden xs:inline',
+    'Skontaktuj się z nami'
+  );
 
   buttonsContainer.appendChild(buttonContact);
   buttonsContainer.appendChild(buttonOffer);
