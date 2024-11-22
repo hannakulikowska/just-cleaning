8528816cd1d4bf17861941a49ce21e7139a408cf
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-11-22 18:07:17 +0100

    refactor: replace <button> with <a> and make component more reusable by adding parameters

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,6 +1,17 @@
-export function createButton() {
-  const button = document.createElement('button');
-  button.className = 'btn text-xs sm:text-sm px-[28px] py-[12px] sm:px-10 sm:py-4';
+export function createButton(
+  text: string,
+  href: string,
+  additionalClasses?: string,
+  ariaLabel?: string
+) {
+  const link = document.createElement('a');
+  link.href = href;
+  link.className = `btn text-xs sm:text-sm px-[28px] py-[12px] sm:px-10 sm:py-4 ${additionalClasses}`;
+  link.textContent = text;
 
-  return button;
+  if (ariaLabel) {
+    link.setAttribute('aria-label', ariaLabel);
+  }
+
+  return link;
 }
