779a4dfa8248aabe954a05e4800cd16b303dbafc
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-11-24 10:55:49 +0100

    style: update styles for banner text

--- src/components/banner/text.ts
+++ src/components/banner/text.ts
@@ -11,12 +11,12 @@ export function createText() {
 
   textContainer.className =
     'absolute inset-0 flex flex-col items-center justify-center text-center text-white p-6 pt-12';
-  textWrapper.className = 'relative hidden xs:inline';
-  subtitle.className = 'uppercase font-semibold tracking-widest text-[10px] sm:text-sm';
+  textWrapper.className = 'relative';
+  subtitle.className = 'uppercase font-semibold tracking-widest text-[10px]/5 sm:text-sm/9';
   title.className =
-    'font-quicksand uppercase font-bold text-2xl xs:text-4xl sm:text-5xl md:text-7xl pt-6';
+    'font-quicksand uppercase font-bold tracking-wide text-3xl xs:text-4xl sm:text-5xl lg:text-7xl mt-2';
   content.className =
-    'font-quicksand font-bold text-balance text-sm sm:text-xl md:text-3xl lg:text-4xl pt-2 sm:pt-4';
+    'font-quicksand font-bold text-balance text-base sm:text-xl lg:text-3xl  pt-2 sm:pt-4';
 
   subtitle.textContent = 'Serwis sprzątający';
   title.textContent = 'Pranie mebli tapicerowanych';
@@ -25,7 +25,7 @@ export function createText() {
   icon.src = ShineIcon;
   icon.alt = '';
   icon.className =
-    'absolute top-0 right-0 translate-x-14 md:translate-x-24 -translate-y-1/3 max-w-[40px] md:max-w-full h-auto';
+    'hidden md:inline absolute top-0 right-0 translate-x-14 md:translate-x-24 -translate-y-1/3 max-w-[40px] md:max-w-full h-auto';
 
   textContainer.appendChild(textWrapper);
   textWrapper.appendChild(subtitle);
