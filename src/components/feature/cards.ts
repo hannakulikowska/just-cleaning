d99b6e61a0fc5d6a888a63c62cc00709f53e0710
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-11-26 11:17:14 +0100

    refactor: update gap and remove duplicate css class

--- src/components/feature/cards.ts
+++ src/components/feature/cards.ts
@@ -6,7 +6,7 @@ export function createCards() {
   const list = document.createElement('dl');
 
   container.className = 'mx-auto mt-16 max-w-9xl sm:mt-20 lg:mt-24';
-  list.className = 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-x-8 gap-y-10';
+  list.className = 'grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8';
 
   cardsData.forEach(({ title, text, icon }) => {
     const card = createCard(title, text, icon);
