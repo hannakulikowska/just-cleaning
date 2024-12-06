8bcef18f8a5d514a8ecf570b975caadea78cf92d
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-12-06 13:12:11 +0100

    refactor: update section component - reorder arguments and update css styles

--- src/components/section.ts
+++ src/components/section.ts
@@ -2,15 +2,15 @@ import { appendChildren } from '../utils/appendChildren';
 
 export function createSection(
   children?: HTMLElement | HTMLElement[],
-  additionalClasses?: string,
-  id?: string
+  id?: string,
+  additionalClasses?: string
 ) {
   const section = document.createElement('section');
   const container = document.createElement('div');
 
   section.id = `${id}`;
-  section.className = `w-full py-12 lg:py-32 ${additionalClasses || ''}`;
-  container.className = 'max-w-9xl mx-auto px-2 sm:px-6';
+  section.className = `w-full ${additionalClasses || ''}`;
+  container.className = 'max-w-9xl mx-auto';
 
   if (children) {
     appendChildren(container, children);
