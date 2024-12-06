ff5714fe7dd841c330ec8c6ca935050dca0f05c7
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-12-06 13:52:01 +0100

    fix: fix paths and update css styles

--- src/components/header/nav.ts
+++ src/components/header/nav.ts
@@ -3,7 +3,7 @@ import { createCloseButton } from '../closeButton';
 export function createNav() {
   const nav = document.createElement('nav');
   nav.className =
-    'fixed z-[100] top-0 right-0 translate-x-full lg:translate-x-0 lg:static w-64 lg:w-full lg:max-w-fit h-screen lg:h-full bg-neutral rounded-none lg:rounded-b-3xl pt-4 lg:px-10 lg:pt-0 basis-1/3';
+    'fixed z-[100] top-0 right-0 translate-x-full lg:translate-x-0 lg:static w-64 lg:w-full lg:max-w-fit h-screen lg:h-full bg-white rounded-none lg:rounded-b-3xl pt-4 lg:px-4 lg:pt-0 basis-1/3';
 
   const ul = document.createElement('ul');
   ul.className = 'list-none lg:flex lg:items-center justify-around divide-y lg:divide-y-0';
@@ -12,23 +12,33 @@ export function createNav() {
   const closeButton = createCloseButton();
   nav.appendChild(closeButton);
 
+  const currentPath = window.location.pathname;
+
   const navLinks = [
-    { text: 'Strona główna', href: '/', isCurrent: true },
-    { text: 'Usługi', href: '/#service', isCurrent: false },
-    { text: 'Kontakt', href: '/contact', isCurrent: false },
-  ].map(({ text, href, isCurrent }) => {
+    { text: 'Strona główna', href: '/' },
+    { text: 'Usługi', href: '/service' },
+    { text: 'Cennik', href: '/price' },
+    { text: 'Kontakt', href: '/contact' },
+  ].map(({ text, href }) => {
     const li = document.createElement('li');
-    li.className = 'relative px-6 lg:px-10 py-8 lg:dots group cursor-pointer';
+    li.className = 'relative px-6 py-8 lg:dots group cursor-pointer';
+
     const link = document.createElement('a');
     li.appendChild(link);
     link.href = href;
     link.textContent = text;
+
+    const isCurrent = currentPath === new URL(href, window.location.origin).pathname;
+
     link.className = `font-quicksand uppercase font-bold text-sm tracking-wide text-dark group-hover:text-primary whitespace-nowrap transition-[color] duration-300 ${isCurrent ? 'text-primary' : ''}`;
+
     if (isCurrent) {
       link.setAttribute('aria-current', 'page');
     }
+
     return li;
   });
+
   navLinks.forEach((li) => ul.appendChild(li));
 
   return { nav, closeButton };
